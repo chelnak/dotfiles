3e17b61807201dc46d93556c3d53eef4cba28697
Craig Gumbley <craiggumbley@gmail.com>
2019-08-20 23:23:40 +0100

    Fix install script

--- linux/install.sh
+++ linux/install.sh
@@ -3,18 +3,14 @@
 SOURCE="https://github.com/chelnak/dotfiles"
 TARBALL="$SOURCE/tarball/master"
 TARGET="$HOME/dotfiles"
-TAR_CMD="tar -xzv -C "$TARGET" --strip-components=1 --exclude='{.gitignore}'"
 
-is_executable() {
-  type "$1" > /dev/null 2>&1
-}
+INSTALL="git clone $SOURCE $TARGET"
+UPDATE="git pull"
 
-if is_executable "git"; then
-  CMD="git clone $SOURCE $TARGET"
-fi
-
-if [ -z "$CMD" ]; then
-  echo "No git. Aborting."
+if [ -d "$TARGET" ]
+  echo "Updating dotfiles"
+  cd $TARGET
+  eval "$UPDATE"
 else
   echo "Installing dotfiles..."
   mkdir -p "$TARGET"
