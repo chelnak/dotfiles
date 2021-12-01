890f0e92b2218c140514e63ee93e15ac206ed963
Craig Gumbley <craiggumbley@gmail.com>
2021-12-01 22:11:22 +0000

    remove old config

--- config/powershell/Microsoft.VSCode_profile.ps1
+++
@@ -1,5 +0,0 @@
-if ($ENV:OS -eq "Windows_NT"){
-    . "$($ENV:USERPROFILE)/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
-} else {
-    . "$($ENV:HOME)/.config/powershell/Microsoft.PowerShell_profile.ps1"
-}
